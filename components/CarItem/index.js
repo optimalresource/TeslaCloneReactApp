e94b806f4be96a589926ab06606c087a911d77ad
optimalresource <joptimalresource@live.com>
2021-07-11 19:20:57 +0100

    Second commit

--- components/CarItem/index.js
+++ components/CarItem/index.js
@@ -9,7 +9,7 @@ const CarItem = ({
         <ImageBackground source={require('../../assets/images/ModelS.jpeg')} style={styles.image} />
         <View style={styles.titles}>
             <Text style={styles.title}>Model S</Text>
-            <Text style={styles.subtitle}>Starting at $69,420</Text>
+            <Text style={styles.subtitle}>Starting at $69420</Text>
         </View>
     </View>
 );
