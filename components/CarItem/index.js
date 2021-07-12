17a48f4f27ba8fe20f5dc0b58619282ea86b7b27
optimalresource <joptimalresource@live.com>
2021-07-12 12:32:08 +0100

    Implemented props

--- components/CarItem/index.js
+++ components/CarItem/index.js
@@ -3,29 +3,37 @@ import { Text, View, ImageBackground } from 'react-native';
 import styles from './styles';
 import StyledButton from '../StyledButton';
 
-const CarItem = ({
-    params,
-}) => (
+const CarItem = (props) => {
+    const { name, tagline, taglineCTA, image } = props;
+    return (
     <View style={styles.carContainer}>
-        <ImageBackground source={require('../../assets/images/ModelS.jpeg')} style={styles.image} />
+        <ImageBackground source={image} style={styles.image} />
         <View style={styles.titles}>
-            <Text style={styles.title}>Model S</Text>
-            <Text style={styles.subtitle}>Starting at $69,420</Text>
+            <Text style={styles.title}>{ name }</Text>
+            <Text style={styles.subtitle}>
+                { tagline }
+                <Text style={styles.subtitleCTA}>
+                    { taglineCTA }
+                </Text>
+            </Text>
         </View>
 
-        <StyledButton type='primary'
-            content={'Custom Order'}
-            onPress={() => {
-                console.warn('Custom Order was pressed');
-            }}
-        />
-        <StyledButton type='secondary'
-            content={'Existing Inventory'}
-            onPress={() => {
-                console.warn('Existing Inventory was pressed');
-            }}
-        />
+        <View style={styles.buttonContainer}>
+            <StyledButton type='primary'
+                content={'Custom Order'}
+                onPress={() => {
+                    console.warn('Custom Order was pressed');
+                }}
+            />
+            <StyledButton type='secondary'
+                content={'Existing Inventory'}
+                onPress={() => {
+                    console.warn('Existing Inventory was pressed');
+                }}
+            />
+        </View>
     </View>
-);
+    );
+};
 
 export default CarItem;
